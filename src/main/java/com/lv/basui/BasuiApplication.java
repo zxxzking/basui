881cbb3727867f7499a4a17c0ae452c16e9e8bdd
吕泉 <lvquan@paicaifu.com>
2017-11-14 10:44:39 +0800

    first

---
+++ src/main/java/com/lv/basui/BasuiApplication.java
@@ -0,0 +1,12 @@
+package com.lv.basui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasuiApplication.class, args);
+	}
+}
