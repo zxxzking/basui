40973edb3b9d49c32376e320237ac81c62a8f894
吕泉 <lvquan@paicaifu.com>
2017-12-18 18:03:58 +0800

    content

---
+++ src/main/java/com/lv/basui/service/TitleService.java
@@ -0,0 +1,15 @@
+package com.lv.basui.service;
+
+
+import com.lv.basui.entity.Text;
+import com.lv.basui.entity.Title;
+
+import java.util.List;
+
+public interface TitleService {
+
+
+    List<Title> listTitle();
+
+    Text querySpecificContent(int titleId);
+}
