82ac7a53469cff9f1ce81fa593f3f61a19fabedf
tom <992072389@qq.com>
2020-06-23 13:39:14 +0800

    '123'

--- src/main/java/com/lv/basui/test/Test.java
+++ src/main/java/com/lv/basui/test/Test.java
@@ -1,11 +1,22 @@
 package com.lv.basui.test;
 
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.TreeMap;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("aaa");
+
+    }
+
+    public void test() {
+        String s = "aaa";
+        List<String> list = Lists.newArrayList();
+        list.iterator();
     }
 
 }
